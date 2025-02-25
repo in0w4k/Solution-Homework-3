f5504632aa3e9caad1567644097d66b9b3343c74
in0w4k <in0w4.k@gmail.com>
2025-02-25 19:09:19 +0500

    create Dungeon.java

---
+++ Assignment_1/Dungeon.java
@@ -0,0 +1,22 @@
+package Assignment_1;
+
+import java.util.List;
+
+public class Dungeon {
+    private final String name;
+    private final List<Room> rooms;
+    private final List<NPC> npcs;
+
+    public Dungeon(String name, List<Room> rooms, List<NPC> npcs) {
+        this.name = name;
+        this.rooms = rooms;
+        this.npcs = npcs;
+    }
+
+    public void print() {
+        System.out.println("Dungeon: " + name);
+        System.out.println("Rooms: " + rooms.size());
+        System.out.println("NPCs: " + npcs.size());
+    }
+}
+

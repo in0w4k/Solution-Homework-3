b80b7693e8bdb82e7d15fc25ae68d678e3162662
in0w4k <in0w4.k@gmail.com>
2025-02-25 19:09:31 +0500

    create IDungeonBuilder.java

---
+++ Assignment_1/IDungeonBuilder.java
@@ -0,0 +1,8 @@
+package Assignment_1;
+
+public interface IDungeonBuilder {
+    IDungeonBuilder setDungeonName(String name);
+    IDungeonBuilder addRoom(Room room);
+    IDungeonBuilder addNPC(NPC npc);
+    Dungeon build();
+}
